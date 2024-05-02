94d452acb823485f6ae25b4923871eb112f4a915
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-05-02 23:50:31 +0300

    include fonts & colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
-import $ from "jquery";
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.documentElement.style.setProperty('display', 'block');
+});
 
+// init jquery
+import "./modules/jquery";
+
+// init lazyloading
 import "lazysizes";
 import "lazysizes/plugins/parent-fit/ls.parent-fit";
 
+// primary
 import "../scss/style.scss";
 import "bootstrap";
+
+// modules
+import "./components/lang";
---
+++ assets/js/components/lang.js
@@ -0,0 +1,27 @@
+var icon = $('[language-icon]'),
+    list = $('[language-list]'),
+    item = list.find('input[name="language"]');
+
+class Lang {
+    constructor() {
+        this.events();
+    }
+
+    events() {
+        item.on('change', (e) => {
+            this.changeLanguage(e);
+        });
+    }
+
+    changeLanguage(e) {
+        if ($(e.currentTarget).attr('id') == 'rus_lang') {
+            icon.removeAttr('src')
+            icon.attr('src', '/assets/images/lang/rus.svg');
+        } else {
+            icon.removeAttr('src')
+            icon.attr('src', '/assets/images/lang/eng.svg');
+        }
+    }
+}
+
+new Lang();
