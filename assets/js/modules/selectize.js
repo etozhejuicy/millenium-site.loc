59abd0718e55b33c955194b31ec088df8db25d0c
serj <zenitovic@gmail.com>
2024-05-07 00:50:47 +0300

    fix #2

--- assets/js/modules/selectize.js
+++ assets/js/modules/selectize.js
@@ -3,11 +3,11 @@ import selectize from "@selectize/selectize";
 var selectSelector = $("select");
 
 selectSelector.selectize({
-  placeholder: selectSelector.attr("placeholder"),
-  labelField: 'text',
-  searchField: 'text',
-  sortField: 'text',
+  placeholder: $(this).attr("placeholder"),
+  labelField: "text",
+  searchField: "text",
+  sortField: "text",
   maxItems: 10,
   hideSelected: false,
-  plugins: ['remove_button'],
+  plugins: ["remove_button"],
 });
