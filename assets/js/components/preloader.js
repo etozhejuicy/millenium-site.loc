0805fd91795176cf2631d535068dec045f1acadd
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-05-06 16:04:26 +0300

    create selectize & a lot of fixes

--- assets/js/components/preloader.js
+++ assets/js/components/preloader.js
@@ -10,8 +10,32 @@ class Preloader {
     $('body').addClass('preloader-is-active');
   }
 
+  generate() {
+    let id = $('[preloader-fact-id]'),
+      preloaderTitle = $('[preloader-title]'),
+      preloaderText = $('[preloader-text]'),
+      getId = 1 + Math.floor(Math.random() * 2);
+
+    console.log(preloaderText);
+
+    id.attr('preloader-fact-id', getId)
+
+    if (getId == 1) {
+      var altTitle = 'Ecли вы pacпутaeтe cвoю ДHK, тo oнa oкaжeтcя длинoй в 54, 72 миллиapдa килoмeтpoв';
+      var altText = 'и будeт пpocтиpaтьcя oт Зeмли дo Плутoнa, и oбpaтнo нa Зeмлю 1З paз!';
+    } else {
+      var altTitle = 'Знaeтe ли вы, чтo 99,99% oбычнoй мaтepии cocтoит из пуcтoгo пpocтpaнcтвa?';
+      var altText = 'Ecли вы удaлитe вcё пуcтoe пpocтpaнcтвo, вcя чeлoвeчecкaя paca (нaceлeниe миpa) пoмecтитcя в кpoшeчный куcoчeк caxapa.';
+    }
+
+    preloaderTitle.html(altTitle);
+    preloaderText.html(altText);
+  }
+
   init() {
     if ($("preloader-is-active")) {
+      this.generate();
+      preloader.addClass('preloader-is-animate');
       setTimeout(() => {
         $("body").removeClass("preloader-is-active");
         setTimeout(() => {
--- assets/js/modules/selectize.js
+++ assets/js/modules/selectize.js
@@ -3,8 +3,11 @@ import selectize from "@selectize/selectize";
 var selectSelector = $("select");
 
 selectSelector.selectize({
-  preload: true,
-  persist: false,
   placeholder: selectSelector.attr("placeholder"),
-  hideSelected: true,
+  labelField: 'text',
+  searchField: 'text',
+  sortField: 'text',
+  maxItems: 10,
+  hideSelected: false,
+  plugins: ['remove_button'],
 });
